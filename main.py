0eda4106c7a0bb4c25df68c90bec22c00cb36c14
b166erobot <bernard0freitas@outlook.com>
2023-04-14 20:44:34 -0300

    .

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from src.interface import main
+from contextlib import suppress
+from pathlib import Path
+import tempfile
 
 
-main()
+
+arquivo_lock = Path(tempfile.gettempdir(), 'todo_list.lock')
+if arquivo_lock.exists():
+    print('o programa já esta rodando. saindo.')
+    exit()
+else:
+    with arquivo_lock.open('w') as arquivo:
+        arquivo.write('lock')
+with suppress(KeyboardInterrupt, EOFError):
+    main()
+print('\nprograma finalizado')
+arquivo_lock.unlink()
